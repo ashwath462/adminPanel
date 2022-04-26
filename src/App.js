2f88d5f1ab2c794cdc65b830a04c010c6bf640a7
Ashwath Arora <ashwatharora462@gmail.com>
2022-04-26 22:02:01 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
+import { useState } from "react";
 import { Routes,Route } from "react-router-dom";
 import "./App.css";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import AddProduct from "./pages/AddProduct";
 import Inventory from "./pages/Inventory";
 import UsersPage from "./pages/UsersPage";
 import Home from './pages/Home';
 
 function App() {
+  const [user,SetUserLogin ] = useState(null);
   return (
     <div className="App">
     <Routes>
-    <Route path='/' element={<Home/>}/>
-    <Route path='/addProduct' element={<AddProduct/>}/>
-    <Route path='/inventory' element={<Inventory/>}/>
-    <Route path='/usersPage' element={<UsersPage/>}/>
+      <Route path='/' element={user ? ( <Home/> ) : ( <Login SetUserLogin={SetUserLogin}/> )}/>
+      <Route path='/addProduct' element={user ? ( <AddProduct/> ) : ( <Login SetUserLogin={SetUserLogin}/> )}/>
+      <Route path='/inventory' element={user ? ( <Inventory/> ) : ( <Login SetUserLogin={SetUserLogin}/> )}/>
+      <Route path='/usersPage' element={user ? ( <UsersPage/> ) : ( <Login SetUserLogin={SetUserLogin}/> )}/>
+      <Route path='/register' element={<Register/>}/>
     </Routes>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import "../styles/Home.css"
 export default function Home() {
+
   return (
     <div className='home'>
         <h1>Home</h1>
